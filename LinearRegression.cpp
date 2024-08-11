a0cc8c670c265794856ce921ceec736586d1ff46
Денис Сурнин <trane-101@yandex.ru>
2024-08-11 22:51:29 +0400

    updated

--- LinearRegression.cpp
+++ LinearRegression.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_set>
 #include <Eigen/Dense>
 
 
 class LinearRegression {
 private:
-	Eigen::MatrixXf weights;
+	Eigen::VectorXf weights;
 
 	Eigen::VectorXf w;
-	Eigen::VectorXf b;
+	float b;
 
 public:
 	// Calculate weights and biases by using Normal Equation
 	void fit_normal(const Eigen::MatrixXf& X, const Eigen::VectorXf& y) {
-		size_t r, c = X.size();
+		int r = X.rows();
+		int c = X.cols();
 		Eigen::MatrixXf X_aug(r, c + 1);
 		X_aug << Eigen::VectorXf::Ones(r), X;
 		Eigen::VectorXf weights = (X_aug.transpose() * X_aug).inverse() * X_aug.transpose() * y;
+
+		b = weights[0];
+		w = weights.tail(c);
 	};
 
 	// Prediction for the Normal Equation
 	Eigen::VectorXf predict_normal(const Eigen::MatrixXf X) const {
-		Eigen::VectorXf y_predicted = weights * X;
-		return y_predicted;
+		Eigen::MatrixXf X_aug(X.rows(), X.cols() + 1);
+		X_aug << Eigen::VectorXf::Ones(X.rows()), X;
+		return X_aug * weights;
 	};
 
 	// Calculate weights and biases by using gradient descent
 	void fit_gradient(const Eigen::MatrixXf& X, const Eigen::VectorXf& y, const float alpha) {
-		size_t r, c = X.size();
-		Eigen::VectorXf w;
-		Eigen::VectorXf b;
-		w << Eigen::VectorXf::Ones(r);
-		b << Eigen::VectorXf::Ones(r);
+		int c = X.cols();
+		int r = X.rows();
+		
+		w = Eigen::VectorXf::Ones(c);
+		b = 1.0f;
 
 		for (int i = 0; i != 100; ++i) {
-			Eigen::VectorXf y_pred = X * w + b;
-			Eigen::VectorXf d_w = (1 / r) * X.transpose() * (X * w + b - y_pred);
-			Eigen::VectorXf d_b = (1 / r) * (X * w + b - y_pred);
+			Eigen::VectorXf y_pred = X * w + Eigen::VectorXf::Ones(c) * b;
+			Eigen::VectorXf d_w = (1.0f / r) * X.transpose() * (y_pred - y);
+			float d_b = (1.0f / r) * (y_pred - y).sum();
 
 			w = w - alpha * d_w;
 			b = b - alpha * d_b;
@@ -46,7 +48,6 @@ public:
 	};
 
 	Eigen::VectorXf predict_gradient(const Eigen::MatrixXf& X) {
-		Eigen::VectorXf y_pred = X * w + b;
-		return y_pred;
+		return X * w + Eigen::VectorXf::Ones(X.cols()) * b;
 	};
 };
