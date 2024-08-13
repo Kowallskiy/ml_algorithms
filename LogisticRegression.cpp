55df35f3ca02877970ea32f9360db1117e6cc353
Денис Сурнин <trane-101@yandex.ru>
2024-08-14 00:14:14 +0400

    exp

--- LogisticRegression.cpp
+++ LogisticRegression.cpp
@@ -9,15 +9,19 @@ private:
 
 public:
 
+	float sigmoid(float z) {
+		return 1.0f / (1.0f + std::exp(-z));
+	}
+
 	void fit(const Eigen::MatrixXf& X, const Eigen::VectorXf& y) {
 		Eigen::Index r = X.rows();
 		Eigen::Index c = X.cols();
 
 		this->w << Eigen::VectorXf::Ones(c);
-		this->b = 1;
+		this->b = 1.0f;
 
 		for (int i = 0; i != 100; ++i) {
-			Eigen::VectorXf y_pred = 1 / (1 + exp(-(X * this->w + Eigen::VectorXf::Ones(r) * b)));
+			Eigen::VectorXf y_pred = (X * this->w + Eigen::VectorXf::Ones(r) * b).unaryExpr([this](float z) { return sigmoid(z); });
 		}
 	}
 };
