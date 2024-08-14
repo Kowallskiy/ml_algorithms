1a70116f47f58bab2e3e206cc43da60a27fdcc45
Денис Сурнин <trane-101@yandex.ru>
2024-08-14 22:02:28 +0400

    fixed the numeric type mismatch

--- LogisticRegression.cpp
+++ LogisticRegression.cpp
@@ -1,3 +1,6 @@
+#pragma warning(disable: 5054) // Disable warning C5054
+#pragma warning(disable: 4127)
+
 #include <iostream>
 #include <Eigen/Dense>
 #include <cmath>
@@ -13,7 +16,7 @@ public:
 		return 1.0f / (1.0f + std::exp(-z));
 	}
 
-	int data_labeling(float x) {
+	float data_labeling(float x) {
 		if (x >= 0.5) {
 			return 1;
 		}
@@ -22,18 +25,29 @@ public:
 		}
 	}
 
-	void fit(const Eigen::MatrixXf& X, const Eigen::VectorXf& y) {
+	void fit(const Eigen::MatrixXf& X, const Eigen::VectorXf& y, float alpha = 0.01f) {
 		Eigen::Index r = X.rows();
 		Eigen::Index c = X.cols();
 
-		this->w << Eigen::VectorXf::Ones(c);
+		this->w = Eigen::VectorXf::Ones(c);
 		this->b = 1.0f;
 
 		for (int i = 0; i != 100; ++i) {
 			Eigen::VectorXf y_pred = (X * this->w + Eigen::VectorXf::Ones(r) * b).unaryExpr([this](float z) { return sigmoid(z); });
-			Eigen::VectorXf pred_labels = y_pred.unaryExpr([this](float x) {return data_labeling; });
+			Eigen::VectorXf pred_labels = y_pred.unaryExpr([this](float x) {return data_labeling(x); });
 
+			Eigen::VectorXf dw = (1.0f / r) * X.transpose() * (y_pred - y);
+			float db = (1.0f / r) * (y_pred - y).sum();
 
+			this->w = this->w - alpha * dw;
+			this->b = this->b - alpha * db;
 		}
+	}
+
+
+	Eigen::VectorXf predict(const Eigen::MatrixXf& X) {
+		Eigen::VectorXf predictions = (X * this->w + Eigen::VectorXf::Ones(X.rows()) * this->b).unaryExpr([this](float z) { return sigmoid(z); });
+		Eigen::VectorXf labels = predictions.unaryExpr([this](float x) { return data_labeling(x); });
+		return labels;
 	}
 };
