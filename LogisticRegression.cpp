4b4de81d299e84d5e7d8e8a39e0a69201aa738df
Денис Сурнин <trane-101@yandex.ru>
2024-08-14 20:58:18 +0400

    data labeling

--- LogisticRegression.cpp
+++ LogisticRegression.cpp
@@ -13,6 +13,15 @@ public:
 		return 1.0f / (1.0f + std::exp(-z));
 	}
 
+	int data_labeling(float x) {
+		if (x >= 0.5) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+
 	void fit(const Eigen::MatrixXf& X, const Eigen::VectorXf& y) {
 		Eigen::Index r = X.rows();
 		Eigen::Index c = X.cols();
@@ -22,6 +31,9 @@ public:
 
 		for (int i = 0; i != 100; ++i) {
 			Eigen::VectorXf y_pred = (X * this->w + Eigen::VectorXf::Ones(r) * b).unaryExpr([this](float z) { return sigmoid(z); });
+			Eigen::VectorXf pred_labels = y_pred.unaryExpr([this](float x) {return data_labeling; });
+
+
 		}
 	}
 };
