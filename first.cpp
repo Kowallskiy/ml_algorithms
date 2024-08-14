1fa469887191279d40af01695e7bda79bfbba2b5
Денис Сурнин <trane-101@yandex.ru>
2024-08-14 22:02:40 +0400

    updated the inference

--- first.cpp
+++ first.cpp
@@ -4,32 +4,31 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include "LinearRegression.cpp"
+#include "LogisticRegression.cpp"
 
 
 int main() {
     
-    Eigen::MatrixXf X_train(4, 2);
+    Eigen::MatrixXf X_train(6, 2);
     X_train << 1, 2,
-        2, 3,
+        2, 1,
+        2, 2,
         3, 4,
-        4, 5;
+        4, 3,
+        3, 3;
 
-    Eigen::VectorXf y_train(4);
-    y_train << 3, 4, 5, 6;
+    Eigen::VectorXf y_train(6);
+    y_train << 0, 0, 0, 1, 1, 1;
 
-    Eigen::MatrixXf X_test(3, 2);
-    X_test << 5, 6,
-        6, 7,
-        7, 8;
-
-    Eigen::VectorXf y_test(3);
-    y_test << 7, 8, 9;
+    Eigen::MatrixXf X_test(2, 2);
+    X_test << 1, 1,
+        4, 4;
     
-    LinearRegression model;
+    LogisticRegression model;
 
-    model.fit_normal(X_train, y_train); 
+    model.fit(X_train, y_train); 
 
-    Eigen::VectorXf predictions = model.predict_normal(X_test);
+    Eigen::VectorXf predictions = model.predict(X_test);
 
     std::cout << "Predictions using Normal Equation: " << predictions << "\n";
 
