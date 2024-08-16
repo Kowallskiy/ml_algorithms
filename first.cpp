6933dfa80ff1a51bf74051c355983f2e498318ec
Денис Сурнин <trane-101@yandex.ru>
2024-08-16 17:35:37 +0400

    updated

--- first.cpp
+++ first.cpp
@@ -1,34 +1,38 @@
 ﻿#pragma warning(disable: 5054) // Disable warning C5054
 #pragma warning(disable: 4127)
 
+#define INCLUDE_FIRST_CPP
+
 #include <iostream>
 #include <Eigen/Dense>
-#include "LinearRegression.cpp"
-#include "LogisticRegression.cpp"
+#include "LinearRegression.h"
+
 
 
 int main() {
     
     Eigen::MatrixXf X_train(6, 2);
     X_train << 1, 2,
-        2, 1,
-        2, 2,
+        2, 3,
         3, 4,
-        4, 3,
-        3, 3;
+        4, 5,
+        5, 6,
+        6, 7;
 
     Eigen::VectorXf y_train(6);
-    y_train << 0, 0, 0, 1, 1, 1;
+    y_train << 3, 4, 5, 6, 7, 8;
 
-    Eigen::MatrixXf X_test(2, 2);
-    X_test << 1, 1,
-        4, 4;
+    Eigen::MatrixXf X_test(4, 2);
+    X_test << 7, 8,
+        8, 9,
+        9, 10,
+        10, 11;
     
-    LogisticRegression model;
+    LinearRegression model;
 
-    model.fit(X_train, y_train); 
+    model.fit_gradient(X_train, y_train); 
 
-    Eigen::VectorXf predictions = model.predict(X_test);
+    Eigen::VectorXf predictions = model.predict_gradient(X_test);
 
     std::cout << "Predictions using Normal Equation: " << predictions << "\n";
 
