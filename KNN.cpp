2a2d106d75434e27b3274298c7be96a32627c4aa
Денис Сурнин <trane-101@yandex.ru>
2024-08-19 19:35:30 +0400

    first commit

---
+++ KNN.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+#include <algorithm>
+#include <Eigen/Dense>
+#include "KNN.h"
+
+KNNClassifier::KNNClassifier() {};
+
+void KNNClassifier::fit(const Eigen::MatrixXf& X, const Eigen::VectorXf& y) {
+
+}
