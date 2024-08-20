46ae2c53d91ce933567a9c00368738f26687dd07
Денис Сурнин <trane-101@yandex.ru>
2024-08-20 21:57:43 +0400

    KNN Classifier

--- KNN.h
+++ KNN.h
@@ -1,18 +1,22 @@
 #pragma once
+#pragma warning(disable: 5054) // Disable warning C5054
+#pragma warning(disable: 4127)
 
 #include <Eigen/Dense>
 
 
 class KNNClassifier {
 private:
-	int k;
+	int k_class;
 	Eigen::MatrixXf X_original;
 	Eigen::VectorXf y_original;
 
 public:
+
+	KNNClassifier();
 	void fit(const Eigen::MatrixXf& X, const Eigen::VectorXf& y, int k = 5);
 
 	Eigen::VectorXf calculate_distances(const Eigen::VectorXf& x_test);
 
-	int predict(const Eigen::VectorXf& x_test);
+	float predict(const Eigen::VectorXf& x_test);
 };
