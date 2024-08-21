617770adb3c59ad01019c78fbdde6f0f86449bdd
Денис Сурнин <trane-101@yandex.ru>
2024-08-21 21:42:54 +0400

    first commit

---
+++ KMeans.cpp
@@ -0,0 +1,21 @@
+
+
+#include <Eigen/Dense>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include "KMeans.h"
+
+KMeans::KMeans(int k) : k{k} {};
+
+std::vector<int> getRandomIndices(int numSamples, int k) {
+
+}
+
+void KMeans::fit(Eigen::MatrixXf& X) {
+
+}
+
+Eigen::VectorXf KMeans::predict(Eigen::MatrixXf& X) {
+
+}
