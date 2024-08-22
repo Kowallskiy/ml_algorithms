db5d2ebeeca632dc8485b50b053b22af7f827e0e
Денис Сурнин <trane-101@yandex.ru>
2024-08-22 16:53:47 +0400

    updated

--- KMeans.cpp
+++ KMeans.cpp
@@ -26,15 +26,35 @@ void KMeans::fit(Eigen::MatrixXf& X) {
 
 	std::vector<int> indices = getRandomIndices(numSamples, this->k);
 
+	Eigen::MatrixXf centroids;
+	// add the rows of random indices to the 'centroids'
+
+
 	Eigen::MatrixXf distances(k, numSamples);
 
-	for (int i = 0; i < k; ++i) {
+	for (int i = 0; i < indices.size(); ++i) {
 		for (int j = 0; j < numSamples; ++j) {
-			Eigen::VectorXf diff = X.row(j) - X.row(i);
+			Eigen::VectorXf diff = X.row(j) - X.row(indices[i]);
 
 			distances(i, j) = diff.squaredNorm();
 		}
 	}
+	std::vector<int> labels;
+
+	
+	for (int j = 0; j < numSamples; ++j) {
+		float minVal = std::numeric_limits<float>::max();
+		int curLabel = -1;
+		for (int i = 0; i < this->k; ++i) {
+			if (distances(i, j) < minVal) {
+				minVal = distances(i, j);
+				curLabel = i;
+			 }
+		}
+		labels.push_back(curLabel);
+	}
+	// update the centroids
+	
 }
 
 Eigen::VectorXf KMeans::predict(Eigen::MatrixXf& X) {
