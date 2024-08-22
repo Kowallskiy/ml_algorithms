bd0852926abcc695c91388fad65ef106fe80e4aa
Денис Сурнин <trane-101@yandex.ru>
2024-08-22 13:58:45 +0400

    updated

--- KMeans.h
+++ KMeans.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <Eigen/Dense>
+
+class KMeans {
+private:
+	int k;
+public:
+
+	KMeans(int k);
+
+	void fit(Eigen::MatrixXf& X);
+
+	Eigen::VectorXf predict(Eigen::MatrixXf& X);
+};
