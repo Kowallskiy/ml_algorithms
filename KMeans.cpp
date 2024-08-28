4a7f05c2c82ca55e33cbf38d0ce7bfa1f141ecbb
Денис Сурнин <trane-101@yandex.ru>
2024-08-28 20:38:36 +0400

    fixed k initialization

--- KMeans.cpp
+++ KMeans.cpp
@@ -1,4 +1,5 @@
-
+#pragma warning(disable: 5054) // Disable warning C5054
+#pragma warning(disable: 4127)
 
 #include <Eigen/Dense>
 #include <vector>
@@ -9,7 +10,7 @@
 
 KMeans::KMeans(int k) : k{ k }, centroids{} {};
 
-std::vector<int> getRandomIndices(int numSamples, int k) {
+std::vector<int> getRandomIndices(Eigen::Index numSamples, int k) {
 	std::vector<int> indices(numSamples);
 	std::iota(indices.begin(), indices.end(), 0);
 
@@ -22,58 +23,58 @@ std::vector<int> getRandomIndices(int numSamples, int k) {
 }
 
 void KMeans::fit(Eigen::MatrixXf& X) {
-	int numSamples = X.rows();
-	int numFeatures = X.cols();
+	Eigen::Index numSamples = X.rows();
+	Eigen::Index numFeatures = X.cols();
 
 	std::vector<int> indices = getRandomIndices(numSamples, this->k);
 
-	this->centroids(this->k, numFeatures);
+	this->centroids.resize(this->k, numFeatures);
 	
 	for (int i = 0; i < this->k; ++i) {
 		this->centroids.row(i) = X.row(indices[i]);
 	}
 
-	Eigen::MatrixXf distances(k, numSamples);
+	for (int iter = 0; iter < 10; ++iter) {
+		Eigen::MatrixXf distances(this->k, numSamples);
 
-	for (int i = 0; i < this->k; ++i) {
-		for (int j = 0; j < numSamples; ++j) {
-			Eigen::VectorXf diff = X.row(j) - this->centroids.row(i);
-
-			distances(i, j) = diff.squaredNorm();
-		}
-	}
-	std::vector<int> labels;
-
-	for (int j = 0; j < numSamples; ++j) {
-		float minVal = std::numeric_limits<float>::max();
-		int curLabel = -1;
 		for (int i = 0; i < this->k; ++i) {
-			if (distances(i, j) < minVal) {
-				minVal = distances(i, j);
-				curLabel = i;
-			 }
-		}
-		labels.push_back(curLabel);
-	}
-	
-	Eigen::MatrixXf sumCentroids = Eigen::MatrixXf::Zero(this->k, numFeatures);
-	Eigen::VectorXi countSamplesPerCluster = Eigen::VectorXi::Zero(this->k);
-	for (int i = 0; i < numSamples; ++i) {
-		sumCentroids.row(labels[i]) += X.row(i);
-		countSamplesPerCluster[labels[i]]++;
-	}
+			for (int j = 0; j < numSamples; ++j) {
+				Eigen::VectorXf diff = X.row(j) - this->centroids.row(i);
 
-	for (int i = 0; i < this->k; ++i) {
-		if (countSamplesPerCluster[i] != 0) {
-			centroids.row(i) = sumCentroids.row(i) / countSamplesPerCluster[i];
+				distances(i, j) = diff.squaredNorm();
+			}
+		}
+		std::vector<int> labels;
+
+		for (int j = 0; j < numSamples; ++j) {
+			float minVal = std::numeric_limits<float>::max();
+			int curLabel = -1;
+			for (int i = 0; i < this->k; ++i) {
+				if (distances(i, j) < minVal) {
+					minVal = distances(i, j);
+					curLabel = i;
+				}
+			}
+			labels.push_back(curLabel);
+		}
+
+		Eigen::MatrixXf sumCentroids = Eigen::MatrixXf::Zero(this->k, numFeatures);
+		Eigen::VectorXi countSamplesPerCluster = Eigen::VectorXi::Zero(this->k);
+		for (int i = 0; i < numSamples; ++i) {
+			sumCentroids.row(labels[i]) += X.row(i);
+			countSamplesPerCluster[labels[i]]++;
+		}
+
+		for (int i = 0; i < this->k; ++i) {
+			if (countSamplesPerCluster[i] != 0) {
+				centroids.row(i) = sumCentroids.row(i) / countSamplesPerCluster[i];
+			}
 		}
 	}
-
 }
 
-Eigen::VectorXf KMeans::predict(Eigen::MatrixXf& X) {
-	int numSamples = X.rows();
-	int numFeatures = X.cols();
+Eigen::VectorXi KMeans::predict(Eigen::MatrixXf& X) {
+	Eigen::Index numSamples = X.rows();
 
 	Eigen::MatrixXf distances(this->k, numSamples);
 	for (int i = 0; i < this->k; ++i) {
@@ -84,8 +85,18 @@ Eigen::VectorXf KMeans::predict(Eigen::MatrixXf& X) {
 		}
 	}
 
-	Eigen::VectorXf resultLabels(numSamples);
+	Eigen::VectorXi resultLabels(numSamples);
 	for (int i = 0; i < numSamples; ++i) {
-
+		float minDist = std::numeric_limits<float>::max();
+		int curLabel = -1;
+		for (int j = 0; j < this->k; ++j) {
+			if (distances(j, i) < minDist) {
+				curLabel = j;
+				minDist = distances(j, i);
+			}
+		}
+		resultLabels[i] = curLabel;
 	}
+
+	return resultLabels;
 }
